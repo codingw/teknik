140ff99d06683927145e0efc5100ccf7a4eaea4c
codingw <codingw@yahoo.com>
2013-09-03 22:34:54 +0700

    Create terbilang.php

---
+++ terbilang.php
@@ -0,0 +1,25 @@
+<?php
+function Terbilang($x)
+{
+  $satuan = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+  if ($x < 12)
+    return " " . $satuan[$x];
+  elseif ($x < 20)
+    return Terbilang($x - 10) . "belas";
+  elseif ($x < 100)
+    return Terbilang($x / 10) . " puluh" . Terbilang($x % 10);
+  elseif ($x < 200)
+    return " seratus" . Terbilang($x - 100);
+  elseif ($x < 1000)
+    return Terbilang($x / 100) . " ratus" . Terbilang($x % 100);
+  elseif ($x < 2000)
+    return " seribu" . Terbilang($x - 1000);
+  elseif ($x < 1000000)
+    return Terbilang($x / 1000) . " ribu" . Terbilang($x % 1000);
+  elseif ($x < 1000000000)
+    return Terbilang($x / 1000000) . " juta" . Terbilang($x % 1000000);
+}
+$uang=1000; // variabel berisi bilangan
+echo "<b>".$uang."</b><br>";
+echo Terbilang($uang)." rupiah "; // memanggil function dan menambah kata rupiah
+?>
