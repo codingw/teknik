5ecac4283f2eac45b28943710b62cf778ce53f38
codingw <codingw@yahoo.com>
2013-09-03 22:35:58 +0700

    Create str_replace.php

---
+++ str_replace.php
@@ -0,0 +1,10 @@
+<?php
+	echo str_replace(" ","_","tes streaming ipad");
+?>
+<hr>
+<?php
+$kata1="Belajar PHP sangat susah yaaa !";
+$ubah= str_replace("susah", "mudah", $kata1);
+print "Kalimat '$kata1' <br>akan diganti menjadi : <br> ";
+print $ubah;
+?>
