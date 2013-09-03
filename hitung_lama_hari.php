9c51d8212a333fde9eb48cce0146ce3a5b4748cc
codingw <codingw@yahoo.com>
2013-09-03 22:32:13 +0700

    hitung lama hari

---
+++ hitung_lama_hari.php
@@ -0,0 +1,9 @@
+<?php
+$datetime1 = new DateTime('2009-10-11');
+$datetime2 = new DateTime('2009-10-13');
+$interval = $datetime1->diff($datetime2);
+//echo $interval->format('%R%a days'); //+ 2
+echo $interval->format('%a days');
+echo "<br><br><br>";
+
+?>
